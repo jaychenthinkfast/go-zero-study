d2e7af26426362da2714a3a9564e9c77400426df
Jay Chen <chenjie@chenjie.info>
2025-03-04 09:51:02 +0800

    update db table

--- mall/sql/mall.sql
+++ mall/sql/mall.sql
@@ -68,6 +68,16 @@ CREATE TABLE `category` (
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='商品类别表';
 
+CREATE TABLE `product_operation` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `product_id` bigint unsigned NOT NULL DEFAULT 0 COMMENT '商品id',
+  `status` int NOT NULL DEFAULT '1' COMMENT '运营商品状态 0-下线 1-上线',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `ix_update_time` (`update_time`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COMMENT='商品运营表';
+
 /*========================>database cart <===================================*/
 CREATE DATABASE cart;
 USE cart;
@@ -106,23 +116,23 @@ CREATE TABLE `orders` (
 
 CREATE TABLE `orderitem` (
      `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '订单子表id',
-     `orderid` varchar(64) NOT NULL DEFAULT '' COMMENT '订单id',
-     `userid` bigint(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户id',
-     `proid` bigint(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '商品id',
-     `proname` varchar(100) NOT NULL DEFAULT '' COMMENT '商品名称',
-     `proimage` varchar(500) NOT NULL DEFAULT '' COMMENT '商品图片地址',
-     `currentunitprice` decimal(20,2) NOT NULL DEFAULT 0 COMMENT '生成订单时的商品单价，单位是元,保留两位小数',
+     `order_id` varchar(64) NOT NULL DEFAULT '' COMMENT '订单id',
+     `user_id` bigint(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户id',
+     `product_id` bigint(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '商品id',
+     `product_name` varchar(100) NOT NULL DEFAULT '' COMMENT '商品名称',
+     `product_image` varchar(500) NOT NULL DEFAULT '' COMMENT '商品图片地址',
+     `current_price` decimal(20,2) NOT NULL DEFAULT 0 COMMENT '生成订单时的商品单价，单位是元,保留两位小数',
      `quantity` int(10) NOT NULL DEFAULT 0 COMMENT '商品数量',
-     `totalprice` decimal(20,2) NOT NULL DEFAULT 0 COMMENT '商品总价,单位是元,保留两位小数',
+     `total_price` decimal(20,2) NOT NULL DEFAULT 0 COMMENT '商品总价,单位是元,保留两位小数',
      `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
      `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
      PRIMARY KEY (`id`),
-     KEY `ix_orderid` (`orderid`),
-     KEY `ix_userid` (`userid`),
-     KEY `ix_proid` (`proid`)
+     KEY `ix_orderid` (`order_id`),
+     KEY `ix_userid` (`user_id`),
+     KEY `ix_proid` (`product_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='订单明细表';
 
-CREATE TABLE `shopping` (
+CREATE TABLE `shipping` (
     `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '收货信息表id',
     `orderid` varchar(64) NOT NULL DEFAULT '' COMMENT '订单id',
     `userid` bigint(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户id',
